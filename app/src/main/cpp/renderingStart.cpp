14c395f7c8dabbe0b9d422e068b84c1c481da061
Sergey Loskutnikov <nitche9@gmail.com>
2022-10-27 09:55:39 +0300

    new version AGP

--- app/src/main/cpp/renderingStart.cpp
+++ app/src/main/cpp/renderingStart.cpp
@@ -1,4 +1,4 @@
-// Loskutnikov Games .inc
+// TODO Loskutnikov Games .inc
 
 //#include "mainLos/LosMainVulkan.h"
 //#include "mainLos/assetsLos.h"
@@ -6,18 +6,32 @@
 //import androidndk;
 
 //import "xlocale.h";
-#include <los_android_native_app_glue.h>
-#include <string>
+
+//#include <string>
 
 
 //import std_config;
 //import string;
+//import math;
+//#include <xlocale.h>
+
 //import std;
+//import std_config;
+//import std:locale_h;
+
+//#include <xlocale.h>
+#include <los_android_native_app_glue.h>
+#include "logLos.h"
+#include <string_view>
+
+#include <atomic>;
+#include <thread>;
+#include <vector>;
 //import string;
 //import locale_h;
 //import __locale;
 
-
+//import math;
 //#include <los_android_native_app_glue.h>
 //import android_ndk_main;
 //import ModuleA;
@@ -28,10 +42,78 @@
 //import <los_android_native_app_glue.h>;
 
 
+
+
+
+
+enum class Color {
+     red,
+     green,
+     blue
+};
+
+class Point3D {
+public:
+     int x;
+     int y;
+     int z;
+};
+
+std::string_view toString(Color cdf){
+    switch(cdf){
+
+        using enum Color;
+        case red: return "lodRed";
+        case green: [[likely]] return "losGreen";
+        case blue:  return "losBlue";
+    }
+    return "unknown";
+}
+
+
+
+std::vector<int> myvec{};
+std::atomic<bool> atomicBool{false};
+
+void prepareWork() {
+    myvec.insert(myvec.end(), {0, 1, 0, 3});
+    logRun(" data prepare ! ");
+    atomicBool.store(true);
+    atomicBool.notify_one();
+}
+
+void completeWork() {
+
+    logRun(" Waiting for data   ! ");
+    atomicBool.wait(false);
+    myvec[2] = 2;
+    logRun(" Waiting complete  ");
+    for (auto i : myvec) logRun(" %d ", myvec[i]);
+}
+
+
 void android_main(struct android_app* state)
 {
 
-      std::string adfsdf("sdfasdf");
+      //std::string adfsdf("sdfasdf");
+
+    logRun(" my values ! %d", 3000);
+
+     using enum Color;
+
+    logRun(" my values ! %s", toString(Color::green));
+
+
+     Point3D los3D{3, 6, 10};
+    logRun(" my values ! %d, %d, %d", los3D.x, los3D.y, los3D.z);
+
+
+    std::thread t1(prepareWork);
+    std::thread t2(completeWork);
+
+    t1.join();
+    t2.join();
+
 
 
 //    LosAssetLoaderInit((void *) state->activity->assetManager);
